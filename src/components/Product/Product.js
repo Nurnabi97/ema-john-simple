2e76bfc243886c6a0789e0dc3ce2313d568fb17e
Nurnabi97 <nurnabi97@gmail.com>
2021-02-27 02:45:01 +0600

    2nd

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './product.css';
 
-const Product = (props) => {    
+const Product = (props) => { 
+    console.log(props)
+    const {name, img, seller, price, stock} = props.product   
     return (
+      <div className="product">
         <div>
-            <h4>{props.product.name}</h4>   
-            {/* props accessing shop.js product={pd} */}
+           <img src={img} alt=""/>
         </div>
+        <div className="product-info">
+            <h4 className="product-name">{name}</h4>   
+            {/* props accessing shop.js product={pd} */}
+            <h5>By:{seller}</h5>
+            <h4>${price}</h4>
+            <h6>Only {stock} left in stock -Order soon</h6>
+        </div>
+      </div>
     );
 };
 
